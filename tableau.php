166ecf8b847987cff498058d96cae79ad70160b4
Elwilya <69197349+Elwilya@users.noreply.github.com>
2021-01-08 08:57:15 +0100

    Ajout correction de mercredi

---
+++ tableau.php
@@ -0,0 +1,8 @@
+<?php
+$mesBonnets = [
+    ["bonnet en laine", 10, "Lorem ipsum dolor sit amet."], 
+    ["bonnet en laine bio", 14, "Lorem ipsum dolor sit amet."],
+    ["bonnet en laine et cashmire", 20, "Lorem ipsum dolor sit amet."],
+    ["bonnet arc-en-ciel", 12, "Lorem ipsum dolor sit amet."]
+];
+?>
